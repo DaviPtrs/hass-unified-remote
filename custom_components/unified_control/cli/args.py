15a94326bf6e7bc021e7a435b95ac80990429705
bla <bla>
2020-02-27 04:21:39 -0300

    remote_id -> remote_name on arg parser

--- custom_components/unified_control/cli/args.py
+++ custom_components/unified_control/cli/args.py
@@ -13,7 +13,7 @@ def arg_handler():
                         required= False, help = "Default will be 9510", default="9510")
     # Remote Option
     remote = parser.add_argument_group(title='Remote Parameters') 
-    remote.add_argument('remote_id', metavar="Remote-ID", help='Ex.: \"Unified.Netflix\"')
+    remote.add_argument('remote_name', metavar="Remote-Name", help='Ex.: \"prime_video\"')
     remote.add_argument('remote_action', metavar="Remote-Action", help='Ex.: \"play_pause\"')
 
     return parser.parse_args()
